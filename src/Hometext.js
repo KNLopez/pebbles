ef4b00d9e94fb6529f8c37ec27fa2e148aa3f8a5
KurtLopez <kurtdevph@gmail.com>
2017-07-28 21:15:59 +0800

    join us

--- src/Hometext.js
+++ src/Hometext.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import TechTonic from './logo-header.png';
 import Timer from './Timer';
+import Join from './Join';
 
 class HomeText extends Component {
-    state = {  }
+    constructor(props){
+        super(props);
+        this.state = {
+            joinVisible : false
+        }
+
+        this.hideMOdal = this.hideMOdal.bind(this);
+    }
+
+    hideMOdal(){
+        this.setState({
+            joinVisible: !this.state.joinVisible
+        })
+    }
+
     render() {
         return (
             <div className="HomeText">
                 <h1 className ="title"> Learning is Everywhere! </h1>
                 <div className="subHeader">CATCH US AT<img src={TechTonic}/></div>
                 <p> SMX Convention Center | The Office Project Booth</p>
-                <Timer />
+                <button onClick={this.hideMOdal} className="joinUs animate bounceIn"> Join us </button>
+                {
+                    this.state.joinVisible? 
+                    <div className="backdrop" onClick={this.hideMOdal} >
+                        <Join /> 
+                    </div>: ''
+                }
+                
             </div>
         );
     }
